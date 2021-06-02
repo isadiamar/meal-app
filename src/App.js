a8e58c2bd1d1a08f6150cee20eebac2b7692a83b
Isabel Diaz <66790995+isa98ING@users.noreply.github.com>
2021-06-02 12:28:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-function App() {
+import React from 'react';
+
+import Header from './components/Layout/Header/Header';
+const App =() => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
   );
 }
 
