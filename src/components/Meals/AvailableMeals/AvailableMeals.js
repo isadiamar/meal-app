5709140906c8b34f6879c4e9ea406b3a0208a2e7
Isabel Diaz <isabel.d.m@outlook.com>
2021-07-08 13:39:24 +0200

    Fetch Meals in Firebase

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,37 +1,52 @@
+import { useCallback, useEffect, useState } from "react";
 import styles from "./AvailableMeals.module.css";
 
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-const AvailableMeals = () => {
 
-  const mealList = DUMMY_MEALS.map((meal) => (
+const AvailableMeals = () => {
+  //States
+  const [meals, setMeals] = useState([]);
+
+  //Fetch Meals
+  const fetchMeals = useCallback(async()=>{
+    try{
+      //Fetch the meals
+      const response = await fetch('https://meal-app-77a93-default-rtdb.firebaseio.com/meals.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      //Transform in JSON
+      const data = await response.json();
+      console.log(data);
+
+      //Modificate meals
+      let loadedMeals = []
+
+      for(const key in data){
+        loadedMeals.push({
+          id:key,
+          name:data[key].name,
+          price:data[key].price,
+          description:data[key].description
+        });
+      }
+
+      setMeals(loadedMeals);
+
+    }catch(error){
+      console.log(error.message);
+    }
+
+  }, []);
+
+  //Load meals when refresh page
+  useEffect(()=>{
+    fetchMeals();
+  }, [fetchMeals])
+
+  const mealList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
